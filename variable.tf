4e4c3eb5caa552d69df7ab38bd9adfbd7df70ff1
chiragmalhotravbn <101737410+chiragmalhotravbn@users.noreply.github.com>
2025-09-16 10:00:00 +0000

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,13 @@
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+  default     = "my-resource-group"  # You can override this when running or via tfvars file
+}
+
+variable "location" {
+  description = "The Azure region where the resource group will be created"
+  type        = string
+  default     = "East US"
+}
+
 
